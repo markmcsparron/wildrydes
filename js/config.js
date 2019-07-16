a67da582d52d37870292fc2ca3941ab8e1823a9f
markmcsparron <bigmark444@gmail.com>
2019-07-16 11:25:09 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_N7qS4g0Ko', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'h0e2j1h72ih5ilvdokjbhakk7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
